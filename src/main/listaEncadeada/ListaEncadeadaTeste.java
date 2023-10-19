105d53f6b0a33e7e349a282645626d053e63bec8
Guilherme <guilherme.manzano93@gmail.com>
2023-10-19 00:42:33 -0300

    Create initial linked list data structure

---
+++ src/main/listaEncadeada/ListaEncadeadaTeste.java
@@ -0,0 +1,11 @@
+package main.listaEncadeada;
+
+public class ListaEncadeadaTeste {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        lista.adiciona(1);
+
+        System.out.println(lista);
+    }
+}
